a9f3dcfd5acc2b8d5ea9ebb1217eb52726eb3372
Emmanuel <emmanuelprinceg@gmail.com>
2021-02-14 02:49:21 +0530

    Start from forms

--- project/food/models.py
+++ project/food/models.py
@@ -5,6 +5,7 @@ class item(models.Model):
     item_name = models.CharField(max_length=200)
     item_desc = models.CharField(max_length=200)
     item_price= models.IntegerField()
+    item_image=models.CharField(max_length=500,default="https://cdn.dribbble.com/users/1012566/screenshots/4187820/topic-2.jpg")
     def __str__(self):
         return self.item_name
 
--- project/food/urls.py
+++ project/food/urls.py
@@ -1,5 +1,6 @@
 from . import views
 from django.urls import  path
+app_name ='food'  # Namespacing:to avoid naming errors.
 urlpatterns = [
     path('',views.index,name='index'),
     path('item/',views.item, name='item'),
